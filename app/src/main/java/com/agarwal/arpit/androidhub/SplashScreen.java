30ee8d0559cc936101df3975dff4856241f28ff8
Arpit Agarwal - PWA <arpit.agarwal@edelweissfin.com>
2019-01-31 23:34:13 +0530

    Initial commit Key component - Gradle file implementation

---
+++ app/src/main/java/com/agarwal/arpit/androidhub/SplashScreen.java
@@ -0,0 +1,15 @@
+package com.agarwal.arpit.androidhub;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+    }
+}
