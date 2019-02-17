06e247450c5112af02ad65612ff7fb289a8c8750
Arpit Agarwal - PWA <arpit.agarwal@edelweissfin.com>
2019-02-17 16:52:36 +0530

    Test Commit

--- app/src/main/java/com/agarwal/arpit/androidhub/Utils/StringConstants.java
+++ app/src/main/java/com/agarwal/arpit/androidhub/Utils/StringConstants.java
@@ -2,5 +2,5 @@ package com.agarwal.arpit.androidhub.Utils;
 
 public class StringConstants {
 
-    public final static String RECEIVED_STRING = "RECEIVED_STRING";
+    public final static String RECEIVED_STRING = "RECEIVED_STRING" ;
 }
