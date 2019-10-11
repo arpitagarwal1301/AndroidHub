84b3b583f3e85ad0f659b93e9c69313204b6d5df
arpitagarwal1301 <arpitvinshu@gmail.com>
2019-10-11 22:54:19 +0530

    AppController Removed unused classes

--- app/src/main/java/com/agarwal/arpit/androidhub/AppController.kt
+++ app/src/main/java/com/agarwal/arpit/androidhub/AppController.kt
@@ -2,11 +2,7 @@ package com.agarwal.arpit.androidhub
 
 import android.app.Application
 import android.content.Context
-import android.util.Log.INFO
-import android.util.Log.VERBOSE
 import com.agarwal.arpit.common.logging.HubLog
-import com.crashlytics.android.Crashlytics
-import timber.log.Timber
 
 class AppController : Application() {
 
@@ -36,45 +32,6 @@ class AppController : Application() {
 
     }
 
-
-    /*
-    Class for sending crashes to Firebase Crashlytics in case non-debug builds
-     */
-    private class CustomLogTree : Timber.Tree() {
-
-        override fun isLoggable(tag: String?, priority: Int): Boolean {
-            return priority >= INFO
-        }
-
-        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-            if (isLoggable(tag, priority)) {
-                if (null != t) {
-                    Crashlytics.logException(t)
-                }
-                Crashlytics.log(priority, tag, message)
-            }
-        }
-
-
-    }
-
-    private class CustomDebugTree : Timber.Tree() {
-
-        override fun isLoggable(tag: String?, priority: Int): Boolean {
-            return priority >= VERBOSE
-        }
-
-        override fun log(priority: Int, tag: String?, message: String, t: Throwable?) {
-            if (isLoggable(tag, priority)) {
-                if (null != t) {
-                    Crashlytics.logException(t)
-                }
-                Crashlytics.log(priority, tag, message)
-            }
-        }
-
-
-    }
 
 
 }
